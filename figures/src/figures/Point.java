2f72ba3868ce90711d4f1616293d3dbe522baa5e
MinoUni <maks.reva.ua@gmail.com>
2022-02-04 20:07:39 +0200

    add constructor and getters

--- figures/src/figures/Point.java
+++ figures/src/figures/Point.java
@@ -1,9 +1,21 @@
 package figures;
 
 public class Point {
-
-	public Point() {
-		// TODO Auto-generated constructor stub
+	
+	private double x;
+	private double y;
+	
+	public Point(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}	
 
 }
