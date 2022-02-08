7f10a519cda2fc10611ffc2275f59e1844b0ef17
MinoUni <maks.reva.ua@gmail.com>
2022-02-08 18:34:59 +0200

    implement Triangle.centriod method

--- figures/src/figures/Triangle.java
+++ figures/src/figures/Triangle.java
@@ -1,9 +1,32 @@
 package figures;
 
-public class Triangle {
+public class Triangle extends Figure {
 
-	public Triangle() {
-		// TODO Auto-generated constructor stub
+	private Point pointA, pointB, pointC;
+	
+	public Triangle(Point a, Point b, Point c) {
+		pointA = a;
+		pointB = b;
+		pointC = c;
+	}
+
+	@Override
+	protected Point centriod() {
+		double x = (pointA.getX() + pointB.getX() + pointC.getX()) /3;
+		double y = (pointA.getY() + pointB.getY() + pointC.getY()) /3;
+		return new Point(x,y);
+	}
+
+	@Override
+	protected double area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected boolean isTheSame(Figure figure) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
