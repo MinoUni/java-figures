5c90ca9589b27ba6ef9429dfb4a3750426d2f447
MinoUni <maks.reva.ua@gmail.com>
2022-02-09 13:29:58 +0200

    add Circle degeneracy and area verifications

--- figures/src/figures/Circle.java
+++ figures/src/figures/Circle.java
@@ -1,9 +1,45 @@
 package figures;
 
-public class Circle {
+import static java.lang.Math.PI;
+import static java.lang.Math.pow;
+import java.util.Objects;
 
-	public Circle() {
-		// TODO Auto-generated constructor stub
+public class Circle extends Figure {
+	
+	private Point center;
+	private double radius;
+	
+	public Circle(Point center, double radius) {
+		
+		if (Objects.isNull(center))
+			throw new NullPointerException("Center point is NULL");
+		if (!degeneracyCheck(radius))
+			throw new IllegalArgumentException("Radius = " + radius + " < 0");
+		if (area() < 0)
+			throw new IllegalArgumentException("Area = " + area() + " < 0");
+		this.center = center;
+		this.radius = radius;
+	}
+
+	private boolean degeneracyCheck(double radius) {
+		return radius > 0;
+	}
+
+	@Override
+	protected Point centriod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected double area() {
+		return PI * pow(radius, 2);
+	}
+
+	@Override
+	protected boolean isTheSame(Figure figure) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
